a01376276d2d3d8cfdb0a64d2f0799d19b2a7ec2
Prashwar <prashwarmishra@gmail.com>
2021-03-08 14:16:14 +0530

    update carousel section

--- js/script.js
+++ js/script.js
@@ -1,56 +1,36 @@
-function moveToSelected(element) {
-
-    if (element == "next") {
-      var selected = $(".selected").next();
-    } else if (element == "prev") {
-      var selected = $(".selected").prev();
-    } else {
-      var selected = element;
-    }
-  
-    var next = $(selected).next();
-    var prev = $(selected).prev();
-    var prevSecond = $(prev).prev();
-    var nextSecond = $(next).next();
-  
-    $(selected).removeClass().addClass("selected");
-  
-    $(prev).removeClass().addClass("prev");
-    $(next).removeClass().addClass("next");
-  
-    $(nextSecond).removeClass().addClass("nextRightSecond");
-    $(prevSecond).removeClass().addClass("prevLeftSecond");
-  
-    $(nextSecond).nextAll().removeClass().addClass('hideRight');
-    $(prevSecond).prevAll().removeClass().addClass('hideLeft');
-  
+function moveSelected(elem){
+  if(elem == "next"){
+    var selected = $(".selected").next();
+  }else if(elem == "prev"){
+    var selected = $(".selected").prev();
+  }else {
+    return;
   }
-  
-  // Eventos teclado
-  $(document).keydown(function(e) {
-      switch(e.which) {
-          case 37: // left
-          moveToSelected('prev');
-          break;
-  
-          case 39: // right
-          moveToSelected('next');
-          break;
-  
-          default: return;
-      }
-      e.preventDefault();
-  });
-  
-  $('#carousel div').click(function() {
-    moveToSelected($(this));
-  });
-  
-  $('#prev').click(function() {
-    moveToSelected('prev');
-  });
-  
-  $('#next').click(function() {
-    moveToSelected('next');
-  });
+
+  var next = $(selected).next();
+  var prev=$(selected).prev();
+
+  var nextRight=$(next).next();
+  var prevLeft=$(prev).prev();
+
+  $(selected).removeClass().addClass("selected");
+
+  $(next).removeClass().addClass("next");
+  $(prev).removeClass().addClass("prev");
+
+  $(nextRight).removeClass().addClass("next-right");
+  $(prevLeft).removeClass().addClass("prev-left");
+
+  $(nextRight).nextAll().removeClass().addClass("hideRight");
+  $(prevLeft).prevAll().removeClass().addClass("hideLeft");
+
+}
+
+$('#car-prev').click(function(){
+  moveSelected('prev');
+});
+
+$('#car-next').click(function(){
+  moveSelected('next');
+});
   
